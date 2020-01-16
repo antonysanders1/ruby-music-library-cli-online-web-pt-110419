1d3ef15e95478bb42622c9db8d938babdf546a64
Antony Sanders <antony.devon92@gmail.com>
2020-01-16 02:10:18 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,33 +1,38 @@
 class Genre
-  
-  attr_accessor :name
-  
-  @@all = []
-  
-  def initialize(genre)
-    @name = genre
-    save
-  end 
-  
+
+	extend Concerns::Findable
+
+  attr_accessor :name, :songs
+
+   @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    self.save
+  end
+
   def self.all
     @@all
-  end 
-  
+  end
+
+  def save
+    @@all << self
+  end
+
   def self.destroy_all
     @@all.clear
-  end 
-  
-  def save
-    @@all << self 
-  end  
-  
-  def self.create(genre_name)
-    
-    genre = Genre.new(genre_name)
+  end
+
+  def self.create(name)
+    genre = self.new(name)
     genre.save
     genre
-    
-  end 
-  
-  
-end 
+  end
+
+
+  def artists
+    self.songs.collect {|song| song.artist}.uniq
+  end
+
+end
